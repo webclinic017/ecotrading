10b9c0b610089638d3886312387472d7c835a666
tinphan <tinph.1992@gmail.com>
2023-05-12 16:30:22 +0700

    fix logic filter

--- stocklist/logic.py
+++ stocklist/logic.py
@@ -1,11 +1,6 @@
 import numpy as np
-import talib
 from datetime import datetime, timedelta
-import backtrader as bt
-import backtrader.feeds as btfeed
 import pandas as pd
-import matplotlib.pyplot as plt
-import backtrader.analyzers as btanalyzers
 from portfolio.models import StockPrice,StockPriceFilter, get_all_info_stock_price
 from telegram import Bot
 from django.db.models import F
@@ -21,7 +16,7 @@ def breakout_strategy(df, period, num_raw):
     df['tsl'].fillna(method='ffill', inplace=True)
     df['mavol'] = df.groupby('ticker')['volume'].rolling(window=period, min_periods=1).mean().values
     df = df.drop(['res', 'sup'], axis=1)
-    condition = ((df['date'] == date_fiter) & (df['close'] > df['tsl']) & (df['volume'] > df['mavol']*2)& (df['mavol']>200000))
+    condition = ((df['date'] == date_fiter) & (df['close'] > df['tsl']) & (df['volume'] > df['mavol']*2)& (df['mavol']>100000))
     df['buy_breakout'] = condition
     return df
 
@@ -34,81 +29,21 @@ def filter_stock_daily():
     df = breakout_strategy(df, 20, 25)
     df['buy_signal_num'] = df['buy_breakout'].astype(int)
     ticker_list = []
+    bot = Bot(token='5881451311:AAEJYKo0ttHU0_Ztv3oGuf-rfFrGgajjtEk')
     for ticker, group in df.groupby('ticker'):
         ticker_group = group.head(3)[['buy_signal_num']]
         diff = ticker_group.diff().fillna(0)
         if (diff > 0).any().values[0]:
             ticker_list.append(ticker)
     if len(ticker_list)>0:
-        bot = Bot(token='5881451311:AAEJYKo0ttHU0_Ztv3oGuf-rfFrGgajjtEk')
         bot.send_message(
                 chat_id='-870288807', 
-                text=f"Danh sách cổ phiếu có tín hiệu breakout ngày {date_fiter} là: {'; '.join(ticker_list)}")      
+                text=f"Danh sách cổ phiếu có tín hiệu breakout ngày {date_fiter} là: {'; '.join(ticker_list)}")  
+    else:
+        bot.send_message(
+                chat_id='-870288807', 
+                text=f"Không có cổ phiếu thỏa mãn tiêu chí breakout được lọc trong ngày {date_fiter} ")  
     return ticker_list           
 
-
-
-
-
-
-def custom_date_parser(date_string):
-      return pd.to_datetime(date_string, format='%Y-%m-%d')
-
-# # df = pd.read_csv('test.csv', parse_dates=['date'], date_parser=custom_date_parser)
-
-
-
-
-
-
-
-
-# test chiến lược
-
-# class PandasData(btfeed.PandasData):
-#     lines = ('tsl',) # thêm attribute 'tsl' vào lines
-#     params = (
-#         ('datetime', 'Date'),
-#         ('open', 'Open'),
-#         ('high', 'High'),
-#         ('low', 'Low'),
-#         ('close', 'Close'),
-#         ('volume', 'Volume'),
-#         ('openinterest', 'Open Interest'),
-#         ('tsl', 'tsl'),
-#     )
-
-# data = PandasData(dataname=df)
-# cerebro = bt.Cerebro()
-# cerebro.adddata(data)
-
-# class SmaCross(bt.SignalStrategy):
-#     def __init__(self):
-#         self.buy_signal = bt.indicators.CrossOver(self.data.close, self.data.tsl)
-#         sma = bt.indicators.SimpleMovingAverage(self.data.close, period=200)
-#         tsl = bt.indicators.SimpleMovingAverage(self.data.tsl, period=10)
-#         sma.plotinfo.plotname = 'mysma'
-#         tsl.plotinfo.plotname = 'mytsl'
-#     def next(self):
-#         if not self.position:
-#             if self.buy_signal > 0:
-#                 self.buy()
-#         elif self.position:
-#             if self.buy_signal < 0:
-#                 self.close()      
-  
-
-   
-
-# cerebro.addstrategy(SmaCross)
-# # Analyzer
-# cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
-
-# thestrats = cerebro.run()
-# thestrat = thestrats[0]
-
-# print('Sharpe Ratio:', thestrat.analyzers.mysharpe.get_analysis())
-# cerebro.run()
-# cerebro.plot()
 
 
