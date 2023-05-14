c45a9a08058da2d8bcdd733275ce9aeebf9db000
tinphan <tinph.1992@gmail.com>
2023-05-14 23:27:58 +0700

    add model stocklist

--- stocklist/models.py
+++ stocklist/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-
+class Signaldaily(models.Model):
+    ticker = models.CharField(max_length=10)
+    date = models.DateField()#auto_now_add=True)
+    strategy = models.CharField(max_length=50)
+    def __str__(self):
+        return str(self.ticker) + str(self.strategy)
 
 
