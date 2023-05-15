aff5d6e1da0fd3088c7f61139b122546e761d35e
tinphan <tinph.1992@gmail.com>
2023-05-15 09:59:59 +0700

    change field models Signal

--- stocklist/models.py
+++ stocklist/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Signaldaily(models.Model):
     ticker = models.CharField(max_length=10)
     date = models.DateField()#auto_now_add=True)
+    signal = models.CharField(max_length=10)
     strategy = models.CharField(max_length=50)
     def __str__(self):
         return str(self.ticker) + str(self.strategy)
