ea3e22c85eaead942ee7edd8828b47d7836b42f4
tinphan <tinph.1992@gmail.com>
2023-06-23 10:18:21 +0700

    fix bug

--- stocklist/models.py
+++ stocklist/models.py
@@ -47,9 +47,8 @@ class Signaldaily(models.Model):
     
 @receiver(post_save, sender=StockPriceFilter)
 def create_cutloss_signal(sender, instance, created, **kwargs):
-    if created:
-        signal  = Signaldaily.objects.filter(ticker=instance.ticker, strategy=1)  
-        #, is_cutloss=False 
+    if not created:
+        signal  = Signaldaily.objects.filter(ticker=instance.ticker, strategy=1,is_cutloss=False )  
         for stock in signal:
             stock.cutloss_price = round(stock.close*(100-stock.ratio_cutloss),2)
             if stock.cutloss_price >= instance.close:
