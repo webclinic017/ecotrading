8254c3ab31d722ea4189bab54fa825509219fba5
tinphan <tinph.1992@gmail.com>
2023-08-16 13:24:59 +0700

    fix bug

--- stocklist/views.py
+++ stocklist/views.py
@@ -238,12 +238,12 @@ def define_stock_not_test(strategy):
 
 def run_backtest(risk, begin_list, end_list):
     strategy_data = {
-        'name': 'Breakout',
+        'name': 'Breakout ver 0.2',
         'risk': risk,   
         'nav': 10000000,
         'commission' : 0.0015,
         'period':20}
-    created = StrategyTrading.objects.update_or_create(name='Breakout ver 0.2',risk = risk, defaults=strategy_data)
+    created = StrategyTrading.objects.update_or_create(name=strategy_data['name'],risk = risk, defaults=strategy_data)
     strategy = StrategyTrading.objects.filter(name='Breakout ver 0.2',risk = risk).first()
     stock_source = StockPriceFilter.objects.values('ticker').annotate(avg_volume=Avg('volume'))
     stock_test= [ticker for ticker in stock_source if ticker['avg_volume'] > 100000]
