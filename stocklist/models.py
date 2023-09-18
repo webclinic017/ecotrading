8afb3cd5844472e28001512f5117085d9e518185
tinphan <tinph.1992@gmail.com>
2023-09-18 13:42:44 +0700

    add tenisball_backtest

--- stocklist/models.py
+++ stocklist/models.py
@@ -268,12 +268,12 @@ class RatingStrategy(models.Model):
 class ParamsOptimize(models.Model):  
     ticker = models.CharField(max_length=15,  verbose_name = 'Cổ phiếu' )  
     strategy = models.ForeignKey(StrategyTrading,on_delete=models.CASCADE, null=True, blank=True,verbose_name = 'Chiến lược' )
-    param1 = models.FloatField(default=2, verbose_name='Cấp nhân khối lượng') #mul
-    param2 = models.FloatField(default=0.03 , verbose_name = 'Biến động tăng') #rate of
-    param3 = models.FloatField(default=0.015, verbose_name = 'Tỷ lệ nến') #change
-    param4= models.FloatField(default=0.05 , verbose_name = 'Tỷ lệ cắt lỗ') #ratio_cut
-    param5 = models.FloatField(default=20 , verbose_name = 'Trung bình giá') #sma
-    param6  =models.IntegerField(default=0 , verbose_name = 'Ngày tích lũy') #len
+    param1 = models.FloatField(default=2) #, verbose_name='Cấp nhân khối lượng') #mul
+    param2 = models.FloatField(default=0.03) # , verbose_name = 'Biến động tăng') #rate of
+    param3 = models.FloatField(default=0.015) #, verbose_name = 'Tỷ lệ nến') #change
+    param4= models.FloatField(default=0.05) # , verbose_name = 'Tỷ lệ cắt lỗ') #ratio_cut
+    param5 = models.FloatField(default=20) # , verbose_name = 'Trung bình giá') #sma
+    param6  =models.IntegerField(default=0) #, verbose_name = 'Ngày tích lũy') #len
 
     def __str__(self):
         return self.ticker
