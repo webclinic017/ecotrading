8e993ed6e12bc1323b2f4418649af6aeab196084
tinphan <tinph.1992@gmail.com>
2023-12-14 15:56:27 +0700

    fix bug ver 0.3

--- stockwarehouse/models.py
+++ stockwarehouse/models.py
@@ -154,7 +154,7 @@ class Transaction (models.Model):
         ratio_requirement = self.stock.initial_margin_requirement/100
 
         if self.position == 'buy': 
-            max_qty = abs((account.nav/ratio_requirement/account.margin_ratio)/self.price)
+            max_qty = abs((account.nav/(ratio_requirement*account.margin_ratio))/self.price)
             if self.qty > max_qty :
                 raise ValidationError({'qty': f'Không đủ sức mua, số lượng cổ phiếu tối đa  {max_qty:,.0f}'})
                    
