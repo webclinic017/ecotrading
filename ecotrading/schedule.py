a10affb66bfe15afaf9978bbcc2882013955919b
tinphan <tinph.1992@gmail.com>
2023-12-20 16:18:08 +0700

    fix bug

--- ecotrading/schedule.py
+++ ecotrading/schedule.py
@@ -8,26 +8,46 @@ from portfolio.models import get_all_info_stock_price
 def schedule_morning():
     today = datetime.now().date()
     not_trading_dates = DateNotTrading.objects.filter(date=today)
-    # check_update_analysis_and_send_notifications()
-    save_data()
-    auto_news_stock_worlds()
-    morning_check()
+
+    # try:
+    #     # Uncomment nếu bạn có một hàm check_update_analysis_and_send_notifications
+    #     # check_update_analysis_and_send_notifications()
+    # except Exception as e_check_update:
+    #     print(f"An error occurred while running check_update_analysis_and_send_notifications: {e_check_update}")
+
+    try:
+        save_data()
+    except Exception as e_save_data:
+        print(f"An error occurred while running save_data: {e_save_data}")
+
+    try:
+        auto_news_stock_worlds()
+    except Exception as e_auto_news:
+        print(f"An error occurred while running auto_news_stock_worlds: {e_auto_news}")
+
+    try:
+        morning_check()
+    except Exception as e_morning_check:
+        print(f"An error occurred while running morning_check: {e_morning_check}")
+
     if not not_trading_dates:
-        # Thực hiện công việc cần làm vào 7h30
-        # Ví dụ: Gửi email
-        # send_mail(
-        #     'Morning Check',
-        #     'Nội dung kiểm tra buổi sáng...',
-        #     'from@example.com',
-        #     ['to@example.com'],
-        #     fail_silently=False,
-        # )
-        
-        check_dividend()
-        
-        
+        try:
+            # Thực hiện công việc cần làm vào 7h30
+            # Ví dụ: Gửi email
+            # send_mail(
+            #     'Morning Check',
+            #     'Nội dung kiểm tra buổi sáng...',
+            #     'from@example.com',
+            #     ['to@example.com'],
+            #     fail_silently=False,
+            # )
+
+            check_dividend()
+        except Exception as e_check_dividend:
+            print(f"An error occurred while running check_dividend: {e_check_dividend}")
     else:
         pass
+
 
 
 def schedule_mid_trading_date():
@@ -35,9 +55,15 @@ def schedule_mid_trading_date():
     not_trading_dates = DateNotTrading.objects.filter(date=today)
     
     if not not_trading_dates:
-        get_info_stock_price_filter()
-        atternoon_check()
-        
+        try:
+            get_info_stock_price_filter()
+        except Exception as e_get_info_stock:
+            print(f"An error occurred while running get_info_stock_price_filter: {e_get_info_stock}")
+
+        try:
+            atternoon_check()
+        except Exception as e_afternoon_check:
+            print(f"An error occurred while running atternoon_check: {e_afternoon_check}")
     else:
         pass
 
@@ -46,9 +72,14 @@ def schedule_after_trading_date():
     not_trading_dates = DateNotTrading.objects.filter(date=today)
     
     if not not_trading_dates:
-        auto_news_daily()
-        filter_stock_daily()
-        
+        try:
+            auto_news_daily()
+        except Exception as e_auto_news:
+            print(f"An error occurred while running auto_news_daily: {e_auto_news}")
+        try:
+            filter_stock_daily()
+        except Exception as e_filter_stock:
+            print(f"An error occurred while running filter_stock_daily: {e_filter_stock}")
     else:
         pass
 
