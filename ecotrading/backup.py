abeffd32c2e600061cb62e7d716e7d74b4207907
tinphan <tinph.1992@gmail.com>
2023-12-24 20:07:48 +0700

    add backup file

---
+++ ecotrading/backup.py
@@ -0,0 +1,24 @@
+import dropbox
+from django.conf import settings
+import os
+from datetime import datetime as dt
+
+
+def backup_data():
+    # Chạy lệnh sao lưu cơ sở dữ liệu
+    os.system('python manage.py dbbackup')
+    # Đường dẫn đến thư mục lưu trữ sao lưu
+    
+
+    # # Access token từ Dropbox App Console
+    # access_token = 'sl.Bpdff8ea_TpY-mLi7p2hJjzJGj_Pw4YA1Gx2UJkdQBY7FQVMJH_cBs3w2jPnXPh--q6lSKqyiyAjqShLhTNO3v-ERYUzQbU2gYtkJLKThRjhfPD_8ctIt5weED7a7izNJv7nGC2iu71z'
+
+    # # Khởi tạo Dropbox client
+    # dbx = dropbox.Dropbox(access_token)
+
+    # # Tải lên tệp sao lưu lên Dropbox
+    # with open(os.path.join(backup_location, backup_file), 'rb') as f:
+    #     dbx.files_upload(f.read(), f'/DjangoBackups/{backup_file}')
+
+
+
